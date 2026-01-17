a7a07c9f2acff456266a5442c5cab75794e6c9fa
Saurav Singh <sauravcloud26@gmail.com>
2026-01-17 11:52:17 +0530

    print one subsequence with given sum

--- Recursion Practice/sumSubSequence.js
+++ Recursion Practice/sumSubSequence.js
@@ -13,4 +13,25 @@ const printSumSub = (arr, sum, start = 0, temp = [], tempSum = 0) => {
   printSumSub(arr, sum, start + 1, temp, tempSum);
 };
 
-printSumSub([1,2,3,4,5],5)
+const printOneSumSub = (arr, sum, start = 0, temp = [], tempSum = 0) => {
+  if (arr.length == start) {
+    if (tempSum == sum) {
+      console.log(temp);
+      return true;
+    }
+    return false;
+  }
+  temp.push(arr[start]);
+  tempSum += arr[start];
+  if (printOneSumSub(arr, sum, start + 1, temp, tempSum)) {
+    return true;
+  } else {
+    temp.pop();
+    tempSum -= arr[start];
+    return printOneSumSub(arr, sum, start + 1, temp, tempSum);
+  }
+};
+
+printSumSub([1, 2, 3, 4, 5], 5);
+printOneSumSub([1, 2, 3, 4, 5], 5);
+
